5c0393a133e8b312c8c1f8db5d171af8741d9d95
Krzysztof Klinikowski <kkszysiu@gmail.com>
2017-01-29 21:17:46 +0100

    Try to make this compatible with react-native-web

--- src/react-navigation.web.js
+++ src/react-navigation.web.js
@@ -12,9 +12,18 @@ module.exports = {
 
   // Navigators
   get createNavigator() { return require('./navigators/createNavigator').default; },
+  get StackNavigator() { return require('./navigators/StackNavigator').default; },
+  get TabNavigator() { return require('./navigators/TabNavigator').default; },
+  get DrawerNavigator() { return require('./navigators/DrawerNavigator').default; },
 
   // Routers
   get StackRouter() { return require('./routers/StackRouter').default; },
   get TabRouter() { return require('./routers/TabRouter').default; },
 
+  // Views
+  get Transitioner() { return require('./views/Transitioner').default; },
+  get CardStack() { return require('./views/CardStack').default; },
+  get DrawerView() { return require('./views/Drawer/DrawerView').default; },
+  get TabView() { return require('./views/TabView/TabView').default; },
 };
+
